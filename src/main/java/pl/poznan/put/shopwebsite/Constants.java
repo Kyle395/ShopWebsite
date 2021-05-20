c40521404986cc903564a6fc78751d91c0deca41
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2021-05-20 19:29:26 +0200

    Add subs refactor code

--- src/main/java/pl/poznan/put/shopwebsite/Constants.java
+++ src/main/java/pl/poznan/put/shopwebsite/Constants.java
@@ -6,14 +6,14 @@ public class Constants {
 
     public static final boolean DEBUG = true;
 
-    public static final String BULMA_PATH = "css/bulma.min.css";
-    public static final String BULMA_DEBUG_PATH = "css/bulma.css";
+    public static final String BULMA_PATH = "/css/bulma.min.css";
+    public static final String BULMA_DEBUG_PATH = "/css/bulma.css";
 
-    public static final String FONTS_PATH = "css/fa5.min.css";
-    public static final String FONTS_DEBUG_PATH = "css/fa5.css";
+    public static final String FONTS_PATH = "/css/fa5.min.css";
+    public static final String FONTS_DEBUG_PATH = "/css/fa5.css";
 
-    public static final String JQUERY_PATH = "js/jquery-3.6.0.min.js";
-    public static final String JQUERY_DEBUG_PATH = "js/jquery-3.6.0.js";
+    public static final String JQUERY_PATH = "/js/jquery-3.6.0.min.js";
+    public static final String JQUERY_DEBUG_PATH = "/js/jquery-3.6.0.js";
 
     public static void addLibs(Model model) {
         model.addAttribute("bulmaPath", DEBUG ? BULMA_DEBUG_PATH : BULMA_PATH);
