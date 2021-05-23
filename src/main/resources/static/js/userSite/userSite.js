4884851cc030e28dcb3627b80e5dee13fc8b6e5e
JeezChanka <mateus-kaja@wp.pl>
2021-05-23 15:44:33 +0200

    UserSite 1.0.1

--- src/main/resources/static/js/userSite/userSite.js
+++ src/main/resources/static/js/userSite/userSite.js
@@ -1,6 +1,6 @@
-let orderList = [
-    { id: 1, date: "22-04-2020", total: 420.50 }
-];
+let orderList = [];
+
+orderList.push({ id:1, date:"22-04-21", total:420.50 }, { id:2, date:"21-04-21", total:50 });
 
 window.onload = () => {
     orderContent(orderList);
@@ -10,16 +10,14 @@ function orderContent(orderList) {
     let content = document.getElementById('orderTable');
     let dataHtml = '';
     if (orderList.length > 0) {
-        alert("Kurła cuś zamówiłeś kierowniku");
-        dataHtml+=`<table><tr><td>ID zamówienia</td><td>Data złożenia zamówienia</td>` +
-            `<td>Wartość zamówienia</td></tr>`;
+        dataHtml+=`<table><thead><tr><th>Numer zamówienia</th><th>Data złożenia zamówienia</th>` +
+            `<th> Wartość zamówienia</th></tr></thead><tbody>`;
         for (let order of orderList) {
             dataHtml+=`<tr><td>${order.id}</td><td>${order.date}</td><td>${order.total}</td></tr>`;
         }
-        dataHtml+=`</table>`;
+        dataHtml+=`</tbody></table>`;
     } else {
-        alert("Kurła kierowniku pusta lista zamówień");
-        dataHtml+="<p style=\"font-size:32px;\">Brak zamówień.</p>";
+        dataHtml+="<p style=\"font-size:32px; margin: auto; max-width: 1000px;\">Brak zamówień.</p>";
     }
     console.log(dataHtml);
     content.innerHTML = dataHtml;
