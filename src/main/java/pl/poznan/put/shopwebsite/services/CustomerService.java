065827da4ae3f1c51adb66bc896bec1b98c21271
Fierioziy <konowales@gmail.com>
2021-05-23 21:19:10 +0200

    Login changes

--- src/main/java/pl/poznan/put/shopwebsite/services/CustomerService.java
+++ src/main/java/pl/poznan/put/shopwebsite/services/CustomerService.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.poznan.put.shopwebsite.entities.Customer;
 import pl.poznan.put.shopwebsite.repositories.CustomerRepository;
+import pl.poznan.put.shopwebsite.services.customer.ChangePasswordResult;
 
-import java.util.List;
+import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -13,8 +16,39 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public List<Customer> getCustomers() {
-        return customerRepository.findAll();
+    @Transactional
+    public Optional<Customer> authenticate(String login, String password) {
+        Optional<Customer> targetCustomer = customerRepository.findById(login);
+
+        if (targetCustomer.isEmpty()) {
+            return Optional.empty();
+        }
+
+        // TODO hash password
+        Customer customer = targetCustomer.get();
+        if (!customer.getPassword().equals(password)) {
+            return Optional.empty();
+        }
+
+        return targetCustomer;
+    }
+
+    @Transactional
+    public ChangePasswordResult changePassword(Customer customer, String currentPassword,
+                                               String newPassword, String newPasswordRepeat) {
+        if (!newPassword.equals(newPasswordRepeat)) {
+            return ChangePasswordResult.PASSWORD_NOT_EQUAL;
+        }
+
+        // TODO hash password
+        if (!customer.getPassword().equals(currentPassword)) {
+            return ChangePasswordResult.INVALID_DATA;
+        }
+
+        customer.setPassword(newPassword);
+        customerRepository.saveAndFlush(customer);
+
+        return ChangePasswordResult.OK;
     }
 
 }
---
+++ src/main/java/pl/poznan/put/shopwebsite/services/customer/ChangePasswordResult.java
@@ -0,0 +1,5 @@
+package pl.poznan.put.shopwebsite.services.customer;
+
+public enum ChangePasswordResult {
+    OK, INVALID_DATA, PASSWORD_NOT_EQUAL
+}
