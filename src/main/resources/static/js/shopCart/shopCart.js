36aafdc9a9676b3c6ea5b5408f78d58d4dffc0aa
JeezChanka <mateus-kaja@wp.pl>
2021-06-05 18:09:20 +0200

    shopCart visible changes

--- src/main/resources/static/js/shopCart/shopCart.js
+++ src/main/resources/static/js/shopCart/shopCart.js
@@ -3,16 +3,24 @@ let currentSite = 0;
 function cartContent(cartList) {
     let content = document.getElementById('cartTable');
     let dataHtml = '';
+    let totalsum = 0;
     if (cartList.length > 0) {
         dataHtml+=`<table><thead><tr><th>Produkt</th><th>Cena</th>` +
             `<th>Ilość</th><th>Suma</th></tr></thead><tbody>`;
         for (let product of cartList) {
+            let selectHtml = '<select>';
+            for (let i =1; i<=9; i++) {
+                selectHtml+= `<option${product.quantity === i ? ' selected' : ''}>`+ i +'</option>';
+            }
+            selectHtml+='</select>';
+            let totalHtml = product.quantity * product.price;
+            totalsum+=totalHtml;
             // TODO dodać link do produktu
             dataHtml+=`<tr><td><a href="/userSite/orderDetails?id=${product.id}">${product.name}</a></td><td>${product.price}</td>
-                        <td>${product.quantity}</td><td>${product.total}</td></tr>`;
+                    <td>${selectHtml}</td><td>${totalHtml}</td></tr>`;
         }
-        // dataHtml+=` <tr><th>Całkowita kwota</th><td></td><td></td>
-        //             <th th:text="${orderDetails.getTotal()}"></th></tr></tbody></table>`;
+        dataHtml+=` <tr><th>Całkowita kwota</th><td></td><td></td>
+                     <th>${totalsum}</th></tr></tbody></table>`;
     } else {
         dataHtml+="<p style=\"font-size:32px; margin: auto; max-width: 1000px;\">Brak produktów w koszyku.</p>";
     }
