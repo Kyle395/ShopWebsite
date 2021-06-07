90a13ef7314ebfbc2c5464e47afb82a9bc5dd260
Kyle395 <wojciech.lukasz99@gmail.com>
2021-06-07 12:07:06 +0200

    addProduct final form

--- src/main/java/pl/poznan/put/shopwebsite/Constants.java
+++ src/main/java/pl/poznan/put/shopwebsite/Constants.java
@@ -12,6 +12,9 @@ public class Constants {
     public static final String BULMA_PATH = "/css/bulma.min.css";
     public static final String BULMA_DEBUG_PATH = "/css/bulma.css";
 
+    public static final String BULMA_CAROUSEL_PATH = "/css/bulma-carousel.min.css";
+    public static final String BULMA_CAROUSEL_DEBUG_PATH = "/css/bulma-carousel.min.css";
+
     public static final String FONTS_PATH = "/css/fa5.min.css";
     public static final String FONTS_DEBUG_PATH = "/css/fa5.css";
 
@@ -22,6 +25,7 @@ public class Constants {
 
     public static void addLibs(Model model) {
         model.addAttribute("bulmaPath", DEBUG ? BULMA_DEBUG_PATH : BULMA_PATH);
+        model.addAttribute("bulmaCarouselPath", DEBUG ? BULMA_CAROUSEL_DEBUG_PATH: BULMA_CAROUSEL_PATH);
         model.addAttribute("fontsPath", DEBUG ? FONTS_DEBUG_PATH : FONTS_PATH);
         model.addAttribute("jqueryPath", DEBUG ? JQUERY_DEBUG_PATH : JQUERY_PATH);
     }
